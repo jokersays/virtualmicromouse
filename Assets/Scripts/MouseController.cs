b2fb407018546032139ea2285a776005623743b0
svenja-sch <svenja.schipplock@gmail.com>
2023-07-17 17:41:49 +0200

    movement and rotation

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -4,16 +4,49 @@ using UnityEngine;
 
 public class MouseController : MonoBehaviour
 {
+    private Rigidbody2D body;
+    
+    private static float speed = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float move = Input.GetAxis("Horizontal");
-        GetComponent<Rigidbody2D>().velocity = new Vector3(move * 2, GetComponent<Rigidbody2D>().velocity.y);
+        //rotate object
+        if (Input.GetKeyDown("left"))
+        {
+            body.rotation = (body.rotation+90f)%360;
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            body.rotation = (body.rotation+270f)%360;
+        }
+        
+        //move object
+        float move = Input.GetAxis("Vertical"); //-1 backwards
+        if (move != 0)
+        {
+            switch (body.rotation)
+            {
+                case 0:
+                    body.velocity = move * speed * Vector3.up;
+                    break;
+                case 90:
+                    body.velocity = move * speed * Vector3.left;
+                    break;
+                case 180:
+                    body.velocity = move * speed * Vector3.down;
+                    break;
+                case 270:
+                    body.velocity = move * speed * Vector3.right;
+                    break;
+            }
+        }
+
+        move = 0;
     }
 }
