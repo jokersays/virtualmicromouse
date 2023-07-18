b51ab548c7022aa3f1172620bf50f2871b9989f4
svenja-sch <svenja.schipplock@gmail.com>
2023-07-18 14:50:00 +0200

    Object move with button press

--- Assets/Scripts/AutomaticMode.cs
+++ Assets/Scripts/AutomaticMode.cs
@@ -9,6 +9,29 @@ public class AutomaticMode : MonoBehaviour
     public void OnButtonPress(){
         n++;
         Debug.Log("Button clicked " + n + " times.");
-    } 
+        
+        Rigidbody2D body = GetComponent<Rigidbody2D>();
+        moveObject(body);
+    }
+
+    private void moveObject(Rigidbody2D body)
+    {
+        //move object
+        switch (body.rotation)
+        {
+            case 0:
+                body.position = body.position+Vector2.up;
+                break;
+            case 90:
+                body.position = body.position+Vector2.left;
+                break;
+            case 180:
+                body.position = body.position+Vector2.down;
+                break;
+            case 270:
+                body.position = body.position+Vector2.right;
+                break;
+        }
+    }
     
 }
